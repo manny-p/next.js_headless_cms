749dda43ca1c5c9f57518dde06e81ce5ab107b0c
Manny <parra.manny@gmail.com>
2021-08-11 01:15:49 -0600

    test scrollbar v6.16

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ export default function HomePage() {
                 <link rel="icon" href={'/favicon.ico'}/>
             </Head>
 
-            <Grid className="main-container"
+            <Grid
                 h="100vh"
                 templateRows="repeat(1, 1fr)"
                 templateColumns="repeat(5, 1fr)"
