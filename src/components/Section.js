17b8419abefb53bb34043818be59283c80bbf95d
Manny <parra.manny@gmail.com>
2021-09-26 06:45:10 -0600

    troubleshoot deployment

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,10 +5,16 @@ export default function Section() {
 
     return (
         <section>
-            <div className={`${styles.spacer} ${styles.layer1}`}/>
-            <div className={`${styles.spacer} ${styles.layer2}`}/>
+            <div className={`${styles.spacer} ${styles.layer1}`}>
+                `&apos;`
+            </div>
+            <div className={`${styles.spacer} ${styles.layer2}`}>
+                `&apos;`
+            </div>
             <Blob/>
-            <div className={`${styles.spacer} ${styles.layer3}`}/>
+            <div className={`${styles.spacer} ${styles.layer3}`}>
+                `&apos;`
+            </div>
         </section>
     )
 }
