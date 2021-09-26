7fc912dc3abbdcd452fd04beb03cecb0841d3aa6
Manny <parra.manny@gmail.com>
2021-09-26 06:49:53 -0600

    troubleshoot deployment 1.1

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,14 +6,14 @@ export default function Section() {
     return (
         <section>
             <div className={`${styles.spacer} ${styles.layer1}`}>
-                `&apos;`
+                {' '}
             </div>
             <div className={`${styles.spacer} ${styles.layer2}`}>
-                `&apos;`
+                {' '}
             </div>
             <Blob/>
             <div className={`${styles.spacer} ${styles.layer3}`}>
-                `&apos;`
+                {' '}
             </div>
         </section>
     )
