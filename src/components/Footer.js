f66cf38d35951869d70f9f213f967895084933ab
Manny <parra.manny@gmail.com>
2021-09-29 15:29:08 -0600

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
 
     return (
         <footer className={styles.footer}>
-            <p>Copyright &copy; Manny Parra 2021</p>
+            <p>Copyright &copy; Manny Parra 2020-present</p>
         </footer>
     )
 }
