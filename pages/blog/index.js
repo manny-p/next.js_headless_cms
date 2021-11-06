0283271350729cd9d97efcd1e9e53e730b7cdcda
Manny <parra.manny@gmail.com>
2021-11-06 15:49:31 -0600

    add emoji package, update Author component

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,16 +1,15 @@
 import Layout from '@/components/Layout';
 import Card from '@/components/Card';
+import AuthorIntro from '@/components/blog/AuthorIntro';
+
 
 export default function RenderBlog() {
 
     return (
         <>
-            <Layout
-                title="Blog"
-                description="Manny Parra UI/UX Engineer Blog"
-            >
-                {/*<li style={style}>Blog (WIP)</li>*/}
-                <Card/>
+            <Layout title="Blog" description="Manny Parra UI/UX Engineer Blog">
+                <AuthorIntro/>
+                {/*<Card/>*/}
             </Layout>
         </>
     )
--- src/components/blog/AuthorIntro.js
+++ src/components/blog/AuthorIntro.js
@@ -1,29 +1,38 @@
-import {Row, Col, Image} from '@chakra-ui/react'
+import {Box, Flex, Image} from '@chakra-ui/react'
+// import emoji from 'node-emoji'
+let emoji = require('node-emoji')
+
 
 const AuthorIntro = () => {
-    return (
-        <Row>
-            <h5> Author Intro component</h5>
-            <Col md="8">
-                <div className="mb-4 admin-intro">
-                    <Image
-                        roundedCircle
-                        width={64}
-                        height={64}
-                        className="mr-3"
-                        src="https://via.placeholder.com/150"
-                        alt="Generic placeholder"
-                    />
-                    <div>
-                        <h5 className="font-weight-bold mb-0">Mentorshyp</h5>
-                        <p className="welcome-text">
-                            {/*Welcome*/}
-                        </p>
 
-                    </div>
-                </div>
-            </Col>
-        </Row>
+    const wave = emoji.get('wave')
+    const world = emoji.get('earth_americas')
+
+    const style = {
+        color: 'white'
+    }
+
+    return (
+        <>
+            <Flex style={style}
+                  pt={'.5rem'}
+                  pl={'10rem'}
+                  bg={'rgb(38,38,38)'}
+            >
+                <Image
+                    borderRadius="full"
+                    boxSize="75px"
+                    src="https://avatars.githubusercontent.com/u/48877252?v=4"
+                    alt="Manny Parra"
+                />
+                <Box paddingLeft=".5rem">
+                    {/*<h5 className="">Manny Parra</h5>*/}
+                    <p className="welcome-text">
+                        {wave}{' '}{world}
+                    </p>
+                </Box>
+            </Flex>
+        </>
     )
 }
 
